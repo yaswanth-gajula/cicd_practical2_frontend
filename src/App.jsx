1c16f424bea79b3d44e7e77db595a8dfab34af51
yaswanth-gajula <2300030194cseh@gmail.com>
2025-08-02 10:31:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://localhost:8081';
+const BASE_URL = 'http://localhost:9090/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
